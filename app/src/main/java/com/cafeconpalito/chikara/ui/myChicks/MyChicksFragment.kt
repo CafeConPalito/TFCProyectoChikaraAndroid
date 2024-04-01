7d7e37bdacbcda7fd03374bd4b4f43e6155b6b67
Daniel-Espinosa <anareldis@gmail.com>
2024-04-01 19:25:28 +0200

    Home Menu Funcional

--- app/src/main/java/com/cafeconpalito/chikara/ui/myChicks/MyChicksFragment.kt
+++ app/src/main/java/com/cafeconpalito/chikara/ui/myChicks/MyChicksFragment.kt
@@ -1,4 +1,4 @@
-package com.cafeconpalito.chikara.ui.newChick
+package com.cafeconpalito.chikara.ui.myChicks
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [newChickFragment.newInstance] factory method to
+ * Use the [MyChicksFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class newChickFragment : Fragment() {
+class MyChicksFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class newChickFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_chick, container, false)
+        return inflater.inflate(R.layout.fragment_my_chicks, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class newChickFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment newChickFragment.
+         * @return A new instance of fragment MyChicksFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            newChickFragment().apply {
+            MyChicksFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
