4e29c60dcb7782b2067d029f1df3a6fa79656a4d
Ramiro <112170316+ramirogvalverde@users.noreply.github.com>
2024-04-15 20:47:02 +0200

    seguimos con login

--- app/src/main/java/com/cafeconpalito/chikara/ui/login/LoginActivity.kt
+++ app/src/main/java/com/cafeconpalito/chikara/ui/login/LoginActivity.kt
@@ -3,15 +3,18 @@ package com.cafeconpalito.chikara.ui.login
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.cafeconpalito.chikara.R
 import com.cafeconpalito.chikara.databinding.ActivityLoginBinding
+import com.cafeconpalito.chikara.domain.utils
 import com.cafeconpalito.chikara.ui.register.RegisterActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
 
-
+    val errorColor = ContextCompat.getColor(this, R.color.error_edit_text)
+    val defaultColor = ContextCompat.getColor(this, R.color.default_edit_text)
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +25,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun initUI() {
+        clearError()
         initListeners()
     }
 
@@ -37,12 +41,34 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun logginAction() {
-        if (binding.etUserName.text.isBlank()){
-            //binding.etUserName.setHintTextColor(R.color.error_text)
-        }
-        if (binding.etPassword.text.isBlank()){
-            //binding.etPassword.setHintTextColor(R.color)
+        clearError()
+        if (!chekBlank()){
+            val userInput = binding.etUserName.text.toString()
+            val passwordInput = binding.etPassword.text.toString()
+            val passwordAuthentication = utils.cypherTextToMD5(passwordInput)
+
         }
 
+
+
+    }
+
+    private fun chekBlank(): Boolean {
+        var errorUser = false
+        var errorPassword = false
+        if (binding.etUserName.text.isBlank()){
+            binding.etUserName.setHintTextColor(errorColor)
+            errorUser = true
+        }
+        if (binding.etPassword.text.isBlank()){
+            binding.etPassword.setHintTextColor(errorColor)
+            errorPassword = true
+        }
+        return errorUser || errorPassword
+    }
+
+    private fun clearError() {
+        binding.etUserName.setHintTextColor(defaultColor);
+        binding.etPassword.setHintTextColor(defaultColor);
     }
 }
