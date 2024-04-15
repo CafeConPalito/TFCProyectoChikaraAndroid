b49e9085d1aab17935a87981a39fca9fc63ef66d
Daniel-Espinosa <anareldis@gmail.com>
2024-04-15 19:40:01 +0200

    Loggin iniciando codificacion

--- app/src/main/java/com/cafeconpalito/chikara/ui/login/LoginActivity.kt
+++ app/src/main/java/com/cafeconpalito/chikara/ui/login/LoginActivity.kt
@@ -1,22 +1,48 @@
 package com.cafeconpalito.chikara.ui.login
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.cafeconpalito.chikara.R
+import com.cafeconpalito.chikara.databinding.ActivityLoginBinding
+import com.cafeconpalito.chikara.ui.register.RegisterActivity
 import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnLogin.setOnClickListener { logginAction() }
+        binding.btnRegister.setOnClickListener { goToRegister() }
+
+    }
+
+    private fun goToRegister() {
+        val intent =  Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun logginAction() {
+        if (binding.etUserName.text.isBlank()){
+            //binding.etUserName.setHintTextColor(R.color.error_text)
         }
+        if (binding.etPassword.text.isBlank()){
+            //binding.etPassword.setHintTextColor(R.color)
+        }
+
     }
 }
