8e25d84614d26777bdbc39c85b42ea1c4c11a5ec
Daniel-Espinosa <121551604+Daniel-Espinosa@users.noreply.github.com>
2024-05-13 15:19:56 +0200

    Minor changes
    Clean Imports, Add JavaDoc

--- app/src/main/java/com/cafeconpalito/chikara/domain/useCase/LoginCheckUserUseCase.kt
+++ app/src/main/java/com/cafeconpalito/chikara/domain/useCase/LoginCheckUserUseCase.kt
@@ -4,6 +4,12 @@ import com.cafeconpalito.chikara.domain.repository.LoginRepository
 import javax.inject.Inject
 
 class LoginCheckUserUseCase @Inject constructor(private val repository: LoginRepository) {
+
+    /**
+     * Find if username or email is valid
+     * @param user String Username or Email
+     * @return true if exists
+     */
     suspend operator fun invoke(user:String) = repository.checkUser(user)
     
 }
--- app/src/main/java/com/cafeconpalito/chikara/domain/useCase/RegisterUseCase.kt
+++ app/src/main/java/com/cafeconpalito/chikara/domain/useCase/RegisterUseCase.kt
@@ -1,14 +1,30 @@
 package com.cafeconpalito.chikara.domain.useCase
 
 import com.cafeconpalito.chikara.domain.model.UserDto
-import com.cafeconpalito.chikara.domain.repository.LoginRepository
 import com.cafeconpalito.chikara.domain.repository.RegisterRepository
 import javax.inject.Inject
 
 class RegisterUseCase @Inject constructor(private val repository: RegisterRepository) {
 
+    /**
+     * Find if user name is register
+     * @param userName String User Name to check
+     * @return true if already exist
+     */
     suspend fun userNameExist(userName:String) = repository.userNameExist(userName)
+
+    /**
+     * Find if email is register
+     * @param email String email to check
+     * @return true if already exist
+     */
     suspend fun emailExist(email:String) = repository.emailExist(email)
+
+    /**
+     * Register a user.
+     * @param userDto information from user to register.
+     * @return true if register succeed
+     */
     suspend fun registerUser(userDto: UserDto) = repository.registerUser(userDto)
 
 }
