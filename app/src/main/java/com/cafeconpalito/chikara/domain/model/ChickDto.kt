2dea5d4ace4562feb8e8d78b5f8c1704a2cbd765
Daniel-Espinosa <anareldis@gmail.com>
2024-05-29 19:26:25 +0200

    NewChick
    Corregido error switch

--- app/src/main/java/com/cafeconpalito/chikara/domain/model/ChickDto.kt
+++ app/src/main/java/com/cafeconpalito/chikara/domain/model/ChickDto.kt
@@ -5,6 +5,7 @@ data class ChickDto (
         var _id:String = "",
         val title : String,
         val author : String? = null,
+        val author_name : String? = null,
         val date : String? = null,
         var likes : Long = 0,
         val isprivate : Boolean,
--- app/src/main/java/com/cafeconpalito/chikara/ui/newChick/NewChickFragment.kt
+++ app/src/main/java/com/cafeconpalito/chikara/ui/newChick/NewChickFragment.kt
@@ -8,7 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts.*
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.cafeconpalito.chikara.databinding.FragmentNewChickBinding
@@ -201,7 +201,7 @@ class NewChickFragment : Fragment() {
 
         return ChickDto(
             title = binding.etTitle.text.toString(),
-            isprivate = binding.sIsPrivate.isActivated,
+            isprivate = binding.sIsPrivate.isChecked,
             content = content
         )
 
